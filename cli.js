e68472ff2ef8716a45e4c4db67be78231beef1fb
soleromel <soleromel@gmail.com>
2017-06-15 16:00:46 +0200

    Update cli.js
    option -o handle

--- cli.js
+++ cli.js
@@ -10,12 +10,18 @@ const stripIndent = require('strip-indent');
 const fs = require('fs')
 const path = require('path')
 
+console.log('début process');
+
 const cli = meow(`
-    /** will minify and override all the existing images in all subfolders **/
+    /** by default will minify and override all the existing images in all subfolders **/
 
 	Usage
 	  $ imagemin <path|glob> [--plugin=<name> ...]
 	  $ cat <file> | imagemin > <output>
+	  
+	  Options
+	  -p, --plugin   Override the default plugins
+	  -o, --out-dir  Output directory
 
 	Examples
 	  $ imagemin images/*
@@ -24,9 +30,11 @@ const cli = meow(`
 `, {
     string: [
         'plugin',
+        'out-dir'
     ],
     alias: {
         p: 'plugin',
+        o: 'out-dir'
     }
 });
 
@@ -40,8 +48,8 @@ const DEFAULT_PLUGINS = [
 const requirePlugins = plugins => plugins.map(x => {
     try {
         return require(`imagemin-${x}`)();
-} catch (err) {
-    console.error(stripIndent(`
+    } catch (err) {
+        console.error(stripIndent(`
 			Unknown plugin: ${x}
 
 			Did you forgot to install the plugin?
@@ -49,63 +57,79 @@ const requirePlugins = plugins => plugins.map(x => {
 
 			  $ npm install -g imagemin-${x}
 		`).trim());
-    process.exit(1);
-}
+        process.exit(1);
+    }
 });
 
+const processImagemin = (input, dir, opts) => {
+    imagemin(input, dir, opts)
+        .then(files => {
+            if (!dir && files.length === 0) {
+                return;
+            }
+
+            if (!dir && files.length > 1) {
+                console.error('Cannot write multiple files to stdout');
+                process.exit(1);
+            }
+
+            if (!dir) {
+                process.stdout.write(files[0].data);
+                return;
+            }
+
+            console.log(`${files.length} ${plur('image', files.length)} minified in ${dir}`);
+        })
+        .catch(err => {
+            throw err;
+        });
+}
+
 const run = (input, opts) => {
+
     opts = Object.assign({plugin: DEFAULT_PLUGINS}, opts);
 
     const use = requirePlugins(arrify(opts.plugin));
     const spinner = ora('Minifying images');
 
-    let getAllDirectories = srcpath => {
-        let directories = [srcpath]
-        let getDirectories = srcpath => {
-            let folders = fs.readdirSync(srcpath).filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
-            if (folders.length !== 0) {
-                for (let i = 0; i < folders.length; i++) {
-                    directories.push(srcpath + '/' + folders[i])
-                    getDirectories(srcpath + '/' + folders[i])
+    if (Buffer.isBuffer(input)) {
+        imagemin.buffer(input, {use}).then(buf => process.stdout.write(buf));
+        return;
+    }
+
+    if (!fs.lstatSync(input[0]).isDirectory()) {
+        processImagemin(input, opts.outDir, {use})
+    } else {
+
+        let getAllDirectories = srcpath => {
+            let directories = [srcpath]
+            let getDirectories = srcpath => {
+                let folders = fs.readdirSync(srcpath).filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
+                if (folders.length !== 0) {
+                    for (let i = 0; i < folders.length; i++) {
+                        directories.push(srcpath + '/' + folders[i])
+                        getDirectories(srcpath + '/' + folders[i])
+                    }
                 }
             }
+            getDirectories(srcpath)
+            return directories
         }
-        getDirectories(srcpath)
-        return directories
-    }
-    let dirs = getAllDirectories(input[0]);
+        let dirs = getAllDirectories(input[0]);
 
-    for (let i=0; i < dirs.length; i++) {
-        spinner.start();
+        for (let i = 0; i < dirs.length; i++) {
 
-        input = [dirs[i]+'/*']
-        if (Buffer.isBuffer(input)) {
-            imagemin.buffer(input, {use}).then(buf => process.stdout.write(buf));
-            return;
+            input = [dirs[i] + '/*']
+
+            if (!opts.outDir) {
+                opts.outDir = dirs[i];
+            }
+
+            /**
+             * lancement d'imagemin
+             */
+            processImagemin(input, dirs[i], {use})
         }
-        imagemin(input, dirs[i], {use})
-            .then(files => {
-            if (!dirs[i] && files.length === 0) {
-            return;
-        }
-
-        if (!dirs[i] && files.length > 1) {
-            console.error('Cannot write multiple files to stdout');
-            process.exit(1);
-        }
-
-        if (!dirs[i]) {
-            process.stdout.write(files[0].data);
-            return;
-        }
-
-        spinner.stop();
-        console.log(`${files.length} ${plur('image', files.length)} minified in ${dirs[i]}`);
-    })
-    .catch(err => {
-            spinner.stop();
-        throw err;
-    });
     }
 };
 
